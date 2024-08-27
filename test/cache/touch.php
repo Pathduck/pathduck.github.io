c62bcc28ef26467a96e48b852869022df09b1531
Stian Lund <stianlund@gmail.com>
2024-08-27 16:39:18 +0200

    change to tests

--- test/cache/touch.php
+++ test/cache/touch.php
@@ -3,7 +3,10 @@ $wd = getcwd();
 $image = "$wd/vivaldi.jpg";
 
 ignore_user_abort(true);
-system("touch $image");
+system("touch vivaldi.jpg");
+system("mv style1.css style.tmp; mv style2.css style1.css; mv style.tmp style2.css");
+system("touch style1.css");
+
 header("Location: index.html");
 
 ?>
